0697e7f340d7e6bb0cb9d7cc909bb8a940b55cbd
Ajay Prakash <ajay18215@iiitd.ac.in>
2019-08-31 01:18:59 +0530

    i dont know

--- ecell/ecell/settings.py
+++ ecell/ecell/settings.py
@@ -23,7 +23,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 SECRET_KEY = 'u-ufeu@m^khsn&i%hbmys+t@55&d3i2ktn#2=fx#4uj8*ptfq5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
