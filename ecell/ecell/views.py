7abe847aeb5e72fff6fbfe12b5b801c92195adf8
ajaystark <ajay18215@iiitd.ac.in>
2019-09-03 03:31:09 +0530

    test

--- ecell/ecell/views.py
+++ ecell/ecell/views.py
@@ -11,3 +11,8 @@ class pitchcafe(generic.View):
     template="pitchcafe.html"
     def get(self,request):
         return render(request,self.template)
+
+class test(generic.View):
+    template="test.html"
+    def get(self,request):
+        return render(request,self.template)
